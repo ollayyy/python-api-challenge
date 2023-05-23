1075555b0467a156f79b493bc4406006de536e11
Oliver <ollayeve@gmail.com>
2023-05-22 21:00:46 -0500

    complete

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "e726baad920e1d2df6938c0d5a132ec6"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "4f962cfe818641b1b6ee4c87f04a34ef"
